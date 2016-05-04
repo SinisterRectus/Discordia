34b981b35f1f0faddae0388b923da05b8adf4ba6
SinisterRectus <sinister.rectus@gmail.com>
2016-05-04 13:33:23 -0400

    Color constructor now takes hex or dec

--- discordia/classes/utils/color.lua
+++ discordia/classes/utils/color.lua
@@ -1,24 +1,19 @@
 local bit = require('bit')
 local Color = class('Color')
 
-function Color:__init(r, g, b)
-	self.r = math.clamp(r, 0, 255)
-	self.g = math.clamp(g, 0, 255)
-	self.b = math.clamp(b, 0, 255)
-end
-
-function Color.fromHex(num)
-	if type(num) == 'string' then
-		num = tonumber(num:gsub('#', ''), 16)
+function Color:__init(a, b, c)
+	if a and b and c then
+		self.r = math.clamp(a, 0, 255)
+		self.g = math.clamp(b, 0, 255)
+		self.b = math.clamp(c, 0, 255)
+	elseif a then
+		if type(a) == 'string' then
+			a = tonumber(a:gsub('#', ''), 16)
+		end
+		self.r = bit.band(bit.rshift(a, 16), 0xFF)
+		self.g = bit.band(bit.rshift(a, 8), 0xFF)
+		self.b = bit.band(a, 0xFF)
 	end
-	return Color.fromDec(num)
-end
-
-function Color.fromDec(num)
-	local r = bit.band(bit.rshift(num, 16), 0xFF)
-	local g = bit.band(bit.rshift(num, 8), 0xFF)
-	local b = bit.band(num, 0xFF)
-	return Color(r, g, b)
 end
 
 function Color:__tostring()
