bf2bad862f93b1b8d1cbf4eab47628c9fc815c6f
SinisterRectus <sinister.rectus@gmail.com>
2016-05-08 08:35:38 -0400

    Added Member.nickname attribute

--- discordia/classes/discord/member.lua
+++ discordia/classes/discord/member.lua
@@ -12,6 +12,7 @@ function Member:__init(data, server)
 	self.roles = data.roles -- table of role IDs
 	self.server = server -- object
 	self.status = 'offline' -- string
+	self.nickname = data.nick -- string
 	self.joinedAt = data.joinedAt -- string
 
 	-- don't call update, it gets confused
