191c7be8159074998279c4b7deeaad6d088b2c7c
SinisterRectus <sinister.rectus@gmail.com>
2016-06-21 17:32:42 -0400

    Reduced memory usage by class instances

--- discordia/class.lua
+++ discordia/class.lua
@@ -1,28 +1,58 @@
-local mt = {}
-
-function mt:__call(...)
-	local obj = setmetatable({}, self)
-	if type(obj.__init) == 'function' then
-		obj:__init(...)
-	end
-	return obj
-end
-
-function mt:__tostring()
-	return 'class: ' .. self.__name
-end
-
-local function obj__tostring(self)
-	return 'instance of class: ' .. self.__name
-end
+local create, resume, yield = coroutine.create, coroutine.resume, coroutine.yield
 
 return function(name, ...)
 
 	assert(type(name) == 'string', 'Invalid class name')
 
-	local class = setmetatable({__tostring = obj__tostring}, mt)
+	local class = {}
+	local properties = {}
 
-	for _, base in ipairs({...})  do
+	local objMeta = {
+		__index = function(self, k)
+			if properties[k] then
+				return rawget(self, properties[k])
+			else
+				return class[k]
+			end
+		end,
+		__newindex = function(self, k, v)
+			if properties[k] then
+				rawset(self, properties[k], v)
+			else
+				local i = #self + 1
+				properties[k] = i
+				rawset(self, i, v)
+			end
+		end,
+		__pairs = function(self)
+			local coro = create(function()
+				for k, i in pairs(properties) do
+					yield(k, rawget(self, i))
+				end
+			end)
+			return function()
+				local success, k, v = resume(coro)
+				return k, v
+			end
+		end,
+	}
+
+	setmetatable(class, {
+		__call = function(self, ...)
+			for k, v in pairs(self) do
+				objMeta[k] = objMeta[k] or v
+			end
+			local obj = setmetatable({}, objMeta)
+			if obj.__init then obj:__init(...) end
+			return obj
+		end,
+		__tostring = function(self)
+			return "Class: " .. name
+		end
+	})
+
+	local bases = {...}
+	for _, base in ipairs(bases)  do
 		for k, v in next, base do
 			rawset(class, k, v)
 		end
@@ -30,6 +60,7 @@ return function(name, ...)
 
 	rawset(class, '__name', name)
 	rawset(class, '__index', class)
+	rawset(class, '__bases', bases)
 
 	return class
 
