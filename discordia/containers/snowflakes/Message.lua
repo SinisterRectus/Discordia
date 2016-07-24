e6a7a7b5ef6c604656c21ee83061b027dc422b02
SinisterRectus <sinister.rectus@gmail.com>
2016-07-24 18:10:17 -0400

    Fixed nil handling on message update

--- discordia/containers/snowflakes/Message.lua
+++ discordia/containers/snowflakes/Message.lua
@@ -10,7 +10,7 @@ accessors.guild = function(self) return self.parent.guild end
 
 function Message:__init(data, parent)
 	Snowflake.__init(self, data, parent)
-	self.author = self.client:getUserById(data.author.id) or self.client.users:new(data.author)
+	self.author = self.client.users:get(data.author.id) or self.client.users:new(data.author)
 	self:update(data)
 end
 
@@ -19,12 +19,12 @@ function Message:__tostring()
 end
 
 function Message:update(data)
-	self.tts = data.tts
-	self.type = data.type
-	self.pinned = data.pinned
-	self.content = data.content
-	self.editedTimestamp = data.edited_timestamp
-	self.mentionEveryone = data.mentionEveryone
+	self.tts = data.tts == nil and self.tts or data.tts
+	self.type = data.type == nil and self.type or data.type
+	self.pinned = data.pinned == nil and self.pinned or data.pinned
+	self.content = data.content == nil and self.content or data.content
+	self.editedTimestamp = data.edited_timestamp == nil and self.editedTimestamp or data.edited_timestamp
+	self.mentionEveryone = data.mention_everyone == nil and self.mentionEveryone or data.mention_everyone
 	-- TODO: mentions, embeds, attachments
 end
 
