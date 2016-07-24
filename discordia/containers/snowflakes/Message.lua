8dffa550d1b07a50b3f0d1bd3f71a12aa041b699
SinisterRectus <sinister.rectus@gmail.com>
2016-07-24 18:13:15 -0400

    Implemented message mention handling

--- discordia/containers/snowflakes/Message.lua
+++ discordia/containers/snowflakes/Message.lua
@@ -25,7 +25,30 @@ function Message:update(data)
 	self.content = data.content == nil and self.content or data.content
 	self.editedTimestamp = data.edited_timestamp == nil and self.editedTimestamp or data.edited_timestamp
 	self.mentionEveryone = data.mention_everyone == nil and self.mentionEveryone or data.mention_everyone
+	self:parseMentions(data)
 	-- TODO: mentions, embeds, attachments
+end
+
+function Message:parseMentions(data)
+	local mentions = {users = {}, roles = {}, channels = {}}
+	if data.mentions and #data.mentions > 0 then
+		for _, data in ipairs(data.mentions) do
+			mentions.users[data.id] = self.client.users:get(data.id) or self.client.users:new(data)
+		end
+	end
+	if data.mention_roles and #data.mention_roles > 0 then
+		for _, id in ipairs(data.mention_roles) do
+			mentions.roles[id] = self.guild.roles:get(id)
+		end
+	end
+	for mention in self.content:gmatch('<#.->') do
+		local channel = self.guild.textChannels:get(mention:sub(3, -2))
+		if channel then mentions.channels[channel.id] = channel end
+	end
+	if self.mentionEveryone then
+		mentions.roles[self.guild.id] = self.guild.defaultRole
+	end
+	self.mentions = mentions
 end
 
 return Message
