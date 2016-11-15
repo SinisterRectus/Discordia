2dc54779a2feebc7c3f17c4c12fbe3770a45d33f
SinisterRectus <sinister.rectus@gmail.com>
2016-11-14 23:37:07 -0500

    Added snowflake.timestamp string

--- discordia/containers/Snowflake.lua
+++ discordia/containers/Snowflake.lua
@@ -1,5 +1,7 @@
 local Container = require('../utils/Container')
 
+local date = os.date
+local modf = math.modf
 local format = string.format
 
 local Snowflake, property = class('Snowflake', Container)
@@ -19,11 +21,17 @@ function Snowflake:__eq(other)
 end
 
 local function getCreatedAt(self)
-	local ms = self._id / 2^22 + 1420070400000
-	return ms / 1000 -- return seconds for Lua consistency
+	return (self._id / 2^22 + 1420070400000) / 1000
+end
+
+local function getTimestamp(self)
+	local i, f = modf(self.createdAt)
+	local datetime = date('!%Y-%m-%dT%H:%M:%S', i)
+	return format('%s%s+00:00', datetime, format('%.6f', f):sub(2))
 end
 
 property('id', '_id', nil, 'string', "Snowflake ID for the object")
 property('createdAt', getCreatedAt, nil, 'number', "Unix time in seconds at which the object was created by Discord")
+property('timestamp', getTimestamp, nil, 'string', "ISO 8601 date and time at which the object was created by Discord")
 
 return Snowflake
