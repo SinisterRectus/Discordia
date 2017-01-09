2b327e005d5222f13cc2d281020b05a4ab3d1a61
John Sandler <priciel@protonmail.com>
2017-01-09 17:10:57 +0100

    Added modify value.
    Not added modifyChannelD since the snowflake should not be changed.

--- discordia/containers/Embed.lua
+++ discordia/containers/Embed.lua
@@ -21,15 +21,15 @@ local function getGuild(self)
 	return self._parent._api:getGuildEmbed(self._guild_id)
 end
 
-local function modifyGuild(self, enabled, channel_id)
-	return self._parent._api:modifyGuildEmbed(self._guild_id, {enabled = enabled, channel_id = channel_id})
+local function modifyEnabled(self, enabled)
+	return self._parent._api:modifyGuildEmbed(self._guild_id, {enabled = enabled})
 end
 
-property('enabled', '_enabled', nil, 'string', "Wether the Embed is enabled or not.")
+property('enabled', '_enabled', nil, 'string', "Whether the Embed is enabled or not.")
 property('channel_id', '_channel_id', nil, 'string', "Embed's channel id (if enabled is true).")
 
 method('getGuild', getGuild, nil, "Returns guild's Embed.")
-method('modifyGuild', modifyGuild, nil, "Modifies guild's Embed.")
+method('modifyEnabled', modifyEnabled, 'enabled, "Modifies guilds Embeds Enabled attribute.")
 
 
 return Embed
