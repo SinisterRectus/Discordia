1a7f67f83820cea80c9d9b17c62396a5413a6e3b
John Sandler <priciel@protonmail.com>
2017-01-09 18:04:58 +0100

    Not needed anymore.

--- discordia/client/Multipart.lua
+++ discordia/client/Multipart.lua
@@ -1,16 +1,1 @@
-local Multipart = {}
 
-function Multipart:attach(data, name, field)
-	
-	local boundary = "Discordia"
-	local str = "\r\n--" .. boundary .. "\r\nContent-Disposition: form-data; name=\"" .. (field or "file") .."\""
-
-	str = str .. "; filename=\"" .. name .. "\"\r\nContent-Type: application/octet-stream"
-
-	str = str .. "\r\n\r\n"..data.."\r\n--"..boundary.."--"
-
-	return str
-
-end
-
-return Multipart
