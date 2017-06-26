50db5b383336ae538e18682d3a37c0e6169b67e7
SinisterRectus <sinister.rectus@gmail.com>
2017-06-26 10:10:36 -0400

    Replaced messages OrderedCache with WeakCache

--- libs/containers/abstract/TextChannel.lua
+++ libs/containers/abstract/TextChannel.lua
@@ -1,14 +1,12 @@
 local Channel = require('containers/abstract/Channel')
 local Message = require('containers/Message')
-local OrderedCache = require('utils/OrderedCache')
+local WeakCache = require('utils/WeakCache')
 
 local TextChannel = require('class')('TextChannel', Channel)
 
--- TODO: put "uncached" messges into a weak table or weak cache
-
 function TextChannel:__init(data, parent)
 	Channel.__init(self, data, parent)
-    self._messages = OrderedCache(Message, self)
+    self._messages = WeakCache(Message, self)
 end
 
 return TextChannel
