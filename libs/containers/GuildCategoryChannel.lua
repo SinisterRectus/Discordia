3171ceb46c28bde643aad5906837100669a0d1b5
SinisterRectus <sinister.rectus@gmail.com>
2017-10-06 17:59:47 -0400

    Added textChannels and voiceChannels iterables for categories

--- libs/containers/GuildCategoryChannel.lua
+++ libs/containers/GuildCategoryChannel.lua
@@ -1,9 +1,30 @@
 local GuildChannel = require('containers/abstract/GuildChannel')
+local FilteredIterable = require('iterables/FilteredIterable')
 
-local GuildCategoryChannel = require('class')('GuildCategoryChannel', GuildChannel)
+local GuildCategoryChannel, get = require('class')('GuildCategoryChannel', GuildChannel)
 
 function GuildCategoryChannel:__init(data, parent)
 	GuildChannel.__init(self, data, parent)
+end
+
+function get.textChannels(self)
+	if not self._text_channels then
+		local id = self._id
+		self._text_channels = FilteredIterable(self._parent._text_channels, function(c)
+			return c._parent_id == id
+		end)
+	end
+	return self._text_channels
+end
+
+function get.voiceChannels(self)
+	if not self._voice_channels then
+		local id = self._id
+		self._voice_channels = FilteredIterable(self._parent._voice_channels, function(c)
+			return c._parent_id == id
+		end)
+	end
+	return self._voice_channels
 end
 
 return GuildCategoryChannel
