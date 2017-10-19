a090a7e1de9a2dc2dce80bf58fe15a62f6bc0269
SinisterRectus <sinister.rectus@gmail.com>
2017-10-19 13:20:03 -0400

    Added voice state data validation

--- libs/containers/Guild.lua
+++ libs/containers/Guild.lua
@@ -29,6 +29,7 @@ function Guild:__init(data, parent)
 	self._text_channels = Cache({}, GuildTextChannel, self)
 	self._voice_channels = Cache({}, GuildVoiceChannel, self)
 	self._categories = Cache({}, GuildCategoryChannel, self)
+	self._voice_states = {}
 	if not data.unavailable then
 		return self:_makeAvailable(data)
 	end
@@ -39,12 +40,12 @@ function Guild:_makeAvailable(data)
 	self._roles:_load(data.roles)
 	self._emojis:_load(data.emojis)
 
-	local voice_states = data.voice_states
-	for i, state in ipairs(voice_states) do
-		voice_states[state.user_id] = state
-		voice_states[i] = nil
+	if data.voice_states then
+		local states = self._voice_states
+		for _, state in ipairs(data.voice_states) do
+			states[state.user_id] = state
+		end
 	end
-	self._voice_states = voice_states
 
 	local text_channels = self._text_channels
 	local voice_channels = self._voice_channels
