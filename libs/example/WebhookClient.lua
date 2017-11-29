ea19bb1f1bb229b668ebc8b307e07cf4cd6feee2
Juan <priciel@protonmail.com>
2017-11-29 15:34:44 +0100

    Example updated

--- libs/example/WebhookClient.lua
+++ libs/example/WebhookClient.lua
@@ -5,7 +5,7 @@ local WebhookClient
 client:on("ready", function()
 	WebhookClient = discordia.WebhookClient("id", "token", {name = "Discordia's new era!"})
 	local webhookInstance = client:getWebhook("id")
-	
+
 	p("Getters")
 	print("token: " .. webhookInstance.token .. " from Webhook Instance.")
 	print("token: " .. WebhookClient.token .. " from WebhookClient Instance.\n")
@@ -23,31 +23,34 @@ client:on("ready", function()
 	print("channelId: " .. WebhookClient.channelId .. " from WebhookClient Instance.\n")
 	
 	print("user id from the user object: " .. webhookInstance.user.id .. " from Webhook Instance.")
-	print("user id from the user object: " .. WebhookClient.user.id .. " from WebhookClient Instance.\n")
 	
 	print("username from the user object: " .. webhookInstance.user.username .. " from Webhook Instance.")
-	print("username from the user object: " .. WebhookClient.user.username .. " from WebhookClient Instance.\n")
 
 	print("name: " .. webhookInstance.name .. " from Webhook Instance.")
-	print("name: " .. WebhookClient.name .. " from WebhookClient Instance.\n")
+	--print("name: " .. WebhookClient.name .. " from WebhookClient Instance.\n")
 
 	p("Setters")
-	print("setAvatar (changed to " .. webhookInstance:setAvatar("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLqkouhfu-48ykx-9ldL71nCbb6eEj9sp2y3fUW5Mg9hQtJV86").avatarURL .. " url) from Webhook Instance.")
-	print("setAvatar (changed to " .. WebhookClient:setAvatar("https://media.discordapp.net/attachments/381890411414683648/382873214352883713/unknown.png?width=921&height=167").avatarURL .. " url) from WebhookClient Instance.\n")
+	print("Changing WebhookClient name")
+	webhookInstance:setName("Yes")
 
-	print("setChannelId (changed to " .. webhookInstance:setChannelId("379618743497654275").channelId .. " channelId) from Webhook Instance.")
-	print("setChannelId (changed to " .. WebhookClient:setChannelId("379618743497654275").channelId .. " channelId) from WebhookClient Instance.\n")
-
-	print("setName (changed to " .. webhookInstance:setName("AsyncRocks").name .. " name) from Webhook Instance.")
-	print("setName (changed to " .. WebhookClient:setName("AsyncRocks").name .. " name) from WebhookClient Instance.\n")
+	print("Changing WebhookClient avatar")
+	--webhookInstance:setAvatar("shah.png")
+	
+	print("Changing WebhookClient ChannelId")
+	webhookInstance:setChannelId("352134636702400512")
+	
 
 	p("Functions")
-	print("sent 'blah' and received a Message object. ID is: " .. webhookInstance:send("blah").id .. " from Webhook Instance.") -- send returns two objects: Message and self
-	print("sent 'bleh' and received a Message object. ID is: " .. WebhookClient:send("bleh").id .. " from WebhookClient Instance.\n")
+	WebhookClient:send("Hi")
 
+	--[[
 	WebhookClient:send("Sending a file.", {
-		file = "HelloDiscord.ia"
+		file = "HelloDiscord.ia",
+		wait = true
 	})
+	]]
+	
+
 	-- There's also delete, deleteWithToken, Slack compatible (executeSlackCompatible(body)) - Github compatible (executeGitHubCompatible(body)) webhook functions
 
 	--WebhookClient:setChannelId("352134636702400512"):send("test")
@@ -59,15 +62,14 @@ client:on("ready", function()
 	WebhookClient:setName("AsyncTest")
 
 		Set a new Avatar (accepts url, path to file or base64 encoded data)
-	WebhookClient:setAvatar("https://avatars2.githubusercontent.com/u/8753175?v=4&s=400")
+	WebhookClient:setAvatar("shah.png")
 
-		Changes the webhook to the given channelId (accepts url, path to file or base64 encoded data)
-	WebhookClient:setChannelId("id")
+		Changes the webhook to the given channelId (only works with a Webhook Instance)
+	webhookInstance:setChannelId("id")
 
 		Modify webhook's data (data must be a table)
 	WebhookClient:modify({
 		username = "changed again",
-		channelId = "352134636702400512",
 		avatarURL = "https://avatars2.githubusercontent.com/u/8753175?v=4&s=400"
 	})
 
@@ -75,7 +77,6 @@ client:on("ready", function()
 	WebhookClient:delete()
 
 	
-
 		Embedded message
 	WebhookClient:send("Sending embedded message.", {
 		embeds = {
