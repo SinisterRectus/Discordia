06e13572e24eff8e7328387c2285efd5d557b20e
Juan <priciel@protonmail.com>
2017-11-29 11:52:41 +0100

    Code cleaned and optimized

--- libs/client/WebhookClient.lua
+++ libs/client/WebhookClient.lua
@@ -5,11 +5,9 @@ local ssl = require('openssl')
 local fs = require('fs')
 local pathjoin = require('pathjoin')
 
+local API = require('client/API')
 local Logger = require('utils/Logger')
 local Resolver = require('client/Resolver')
-
-local Webhook = require('containers/Webhook')
-local Message = require('containers/Message')
 
 local Emitter = require('utils/Emitter')
 
@@ -34,6 +32,8 @@ local defaultOptions = {
 	avatar = "",
 	channelId = "",
 	guildId = "",
+	id = "",
+	token = "",
 	_user = {}
 }
 
@@ -66,15 +66,7 @@ end
 
 local function resolveImage(avatar, id)
 	if avatar then
-		if tostring(avatar):match("^(https?:)") then
-			local res, data = request("GET", avatar)
-			if res.code < 300 then
-				return 'data:;base64,' .. base64(data)
-			end
-			return
-		else
-			return format("https://cdn.discordapp.com/avatars/%s/%s.png", id, avatar)
-		end
+		return format("https://cdn.discordapp.com/avatars/%s/%s.png", id, avatar)
 	end
 end
 
@@ -93,24 +85,6 @@ local function parseFile(obj)
 	return file
 end
 
-local function clean(user)
-	local cleanedUser = {}
-	for k, v in pairs(user) do
-		if k:find("_") then
-			cleanedUser[k:gsub("_", "")] = v
-		end
-	end
-	return cleanedUser
-end
-
-local function modify(self, tbl)
-	local data, err = self._api:modifyWebhook(self._id, tbl)
-	if data then 
-		return true
-	else
-		return false, err
-	end
-end
 
 local WebhookClient, get = require('class')('WebhookClient', Emitter)
 WebhookClient.__description = "Represents a Webhook Client."
@@ -118,35 +92,31 @@ WebhookClient.__description = "Represents a Webhook Client."
 function WebhookClient:__init(id, token, options)
 	Emitter.__init(self)
 
+	options = options or {}
+	options["id"] = id
+	options["token"] = token
+
 	self._options = parseOptions(options)
-	self._id = tostring(id)
-	self._token = tostring(token)
 	self._logger = Logger(self._options.logLevel, self._options.dateTime, self._options.logFile)
-	self._api = API
-	self._webhook = ""
+	self._api = API(self)
+	self._api:authenticate(nil, true)
 
 	-- Sync our defaultOptions with the webhook data
-	local webhook, err = self._api:getWebhook(self._id) -- we do not use getWebhookWithToken because it does not return the user object
+	local webhook, err = self._api:getWebhookWithToken(self._options.id, self._options.token)
 	if webhook then
 		for key, value in pairs(webhook) do
 			if type(value) ~= "table" then
 				self._options[key] = value
-			else 
-				for user_key, user_value in pairs(value) do
-					if not self._options[key] then self._options[key] = {} end
-					self._options[key]["_" .. user_key] = user_value
-				end
 			end
 		end
 	end
 
 	if options then
-		modify(self, options)
+		self:modify(options)
 		for k, v in pairs(options) do
 			self._options[k] = v
 		end
 	end
-	self._webhook = Webhook(self._options, self, self._api)
 end
 
 for name, level in pairs(logLevel) do
@@ -159,8 +129,8 @@ for name, level in pairs(logLevel) do
 end
 
 -- Getters
-function get.avatarURL(self, size, ext)
-	return self._options.avatarURL ~= "" and self._options.avatarURL or resolveImage(self._options.avatar, self._id)
+function get.avatarURL(self)
+	return self._options.avatarURL ~= "" and self._options.avatarURL or resolveImage(self._options.avatar, self._options.id)
 end
 
 function get.channelId(self)
@@ -168,11 +138,11 @@ function get.channelId(self)
 end
 
 function get.guildId(self)
-	return self._webhook.guildId
+	return self._options.guildId
 end
 
 function get.id(self)
-	return self._webhook.id
+	return self._options.id
 end
 
 function get.name(self)
@@ -180,72 +150,60 @@ function get.name(self)
 end
 
 function get.token(self)
-	return self._webhook.token
+	return self._options.token
 end
-
-function get.user(self)
-	return clean(self._webhook.user) -- does not return a full user object because it is not linked with Client
-end
-
 
 -- Setters and Modifiers
 function WebhookClient:setAvatar(avatar)
 	self._options.avatarURL = avatar
-	avatar = resolveImage(avatar) or Resolver.base64(avatar) or null
-
-	local bool, err = self:modify({avatar = avatar})
-	if bool then
-		return self
+	avatar = Resolver.base64(avatar) or resolveImage(avatar) or null
+	local data, err = self:modify({avatar = avatar})
+	if data then
+		return true
 	else
-		self:error(err)
+		return nil, err
 	end
 end
 
 function WebhookClient:setChannelId(channelId)
 	self._options.channelId = channelId
-	local bool, err = self:modify({channel_id = channelId})
+	local data, err = self:modify({channel_id = channelId})
 
-	if bool then
-		return self
+	if data then
+		return true
 	else
-		self:error(err)
+		return nil, err
 	end
 end
 
 function WebhookClient:setName(name)
 	self._options.name = name
-	local bool, err = self:modify({name = name})
+	local data, err = self:modify({name = name})
 
-	if bool then
-		return self
+	if data then
+		return true
 	else
-		self:error(err)
+		return nil, err
 	end
 end
 
-function WebhookClient:modify(tbl)
-	assert(type(tbl) == "table", "Invalid modify parameter. Table expected")
-
-	for k, v in pairs(tbl) do
-		self._options[k] = v
-	end
-
-	return modify(self, tbl)
-end
-
-function WebhookClient:modifyWithToken(tbl) 
+function WebhookClient:modify(tbl) 
 	assert(type(tbl) == "table", "Invalid modify parameter. Table expected")
 
 	for k, v in pairs(tbl) do
 		if k ~= "channelId" then -- You can not change the channelId
 			self._options[k] = v
 		else
-			self:info("modifyWithToken does not accept a channelId key. Use 'modify' to change it.")
+			self:info("modify does not accept a channelId key.")
 		end
 	end
-	self._api:modifyWebhookWithToken(self._id, self._token, tbl)
 
-	return self
+	local data, err = self._api:modifyWebhookWithToken(self._options.id, self._options.token, tbl)
+	if data then 
+		return true
+	else
+		return false, err
+	end
 end
 
 -- Rest of functions
@@ -270,32 +228,27 @@ function WebhookClient:send(content, options) -- return message
 	end
 
 	local webhookData = self._options
-	local data, err = self._api:executeWebhook(self._id, self._token, { -- We insert these values manually so that we don't send extra (irrevelant) information
+	local data, err = self._api:executeWebhook(self._options.id, self._options.token, { -- We insert these values manually so that we don't send extra (irrevelant) information
 		content = content,
 		username = webhookData.name,
 		avatar_url = webhookData.avatarURL,
 		tts = webhookData.tts,
 		embeds = webhookData.embeds
-	}, webhookData.file)
+	}, webhookData.file, options.wait)
 
 	if data then
-		return Message(data, self), self
+		return true
 	else
-		return self:error(err)
+		return nil, err
 	end
 end
 
 function WebhookClient:delete()
-	local data, err = self._api:deleteWebhook(self._id)
-	if data then
-		return true
-	else
-		return false, err
-	end
+	return self:deleteWithToken()
 end
 
 function WebhookClient:deleteWithToken()
-	local data, err = self._api:deleteWebhookWithToken(self._id, self._token)
+	local data, err = self._api:deleteWebhookWithToken(self._options.id, self._options.token)
 	if data then
 		return true
 	else
@@ -304,11 +257,21 @@ function WebhookClient:deleteWithToken()
 end
 
 function WebhookClient:executeSlackCompatible(body)
-	self._api:executeSlackCompatibleWebhook(self._id, self._token, body)
+	local data, err = self._api:executeSlackCompatibleWebhook(self._options.id, self._options.token, body)
+	if data then
+		return true
+	else
+		return false, err
+	end
 end
 
 function WebhookClient:executeGitHubCompatible(body)
-	self._api:executeGitHubCompatibleWebhook(self._id, self._token, body)
+	local data, err = self._api:executeGitHubCompatibleWebhook(self._options.id, self._options.token, body)
+	if data then
+		return true
+	else
+		return false, err
+	end
 end
 
 return WebhookClient
