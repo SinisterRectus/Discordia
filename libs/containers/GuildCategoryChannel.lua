3becca67f3291919bffd6409bd9cb4f80b79330d
SinisterRectus <sinister.rectus@gmail.com>
2018-02-13 14:42:55 -0500

    Added GuildCategoryChannel:create[Text|Voice]Channel

--- libs/containers/GuildCategoryChannel.lua
+++ libs/containers/GuildCategoryChannel.lua
@@ -1,10 +1,41 @@
 local GuildChannel = require('containers/abstract/GuildChannel')
 local FilteredIterable = require('iterables/FilteredIterable')
+local enums = require('enums')
+
+local channelType = enums.channelType
 
 local GuildCategoryChannel, get = require('class')('GuildCategoryChannel', GuildChannel)
 
 function GuildCategoryChannel:__init(data, parent)
 	GuildChannel.__init(self, data, parent)
+end
+
+function GuildCategoryChannel:createTextChannel(name)
+	local guild = self._parent
+	local data, err = guild.client._api:createGuildChannel(guild._id, {
+		name = name,
+		type = channelType.text,
+		parent_id = self._id
+	})
+	if data then
+		return guild._text_channels:_insert(data)
+	else
+		return nil, err
+	end
+end
+
+function GuildCategoryChannel:createVoiceChannel(name)
+	local guild = self._parent
+	local data, err = guild.client._api:createGuildChannel(guild._id, {
+		name = name,
+		type = channelType.voice,
+		parent_id = self._id
+	})
+	if data then
+		return guild._voice_channels:_insert(data)
+	else
+		return nil, err
+	end
 end
 
 function get.textChannels(self)
