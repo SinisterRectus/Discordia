3f259b0c183bfc44ecc5885cc526a25987c1c40c
Tigerism <Tigerism@users.noreply.github.com>
2018-04-24 00:08:11 -0400

    Added some examples (#119)
    * Added some examples
    
    * Flushed the lines table
    
    * Removed unused author variable
    
    * Removed unused author variable

---
+++ examples/appender.lua
@@ -0,0 +1,29 @@
+local discordia = require("discordia")
+local client = discordia.Client()
+
+local lines = {} -- blank table of messages
+
+
+client:on("ready", function() -- bot is ready
+	print("Logged in as " .. client.user.username)
+end)
+
+client:on("messageCreate", function(message)
+	local content = message.content
+	local author = message.author
+
+	if author == client.user then return end -- the bot should not append its own messages
+
+	if content == "!lines" then -- if the lines command is activated
+		message.channel:send {
+			file = {"lines.txt", table.concat(lines, "\n")} -- concatenate the lines table and reply with a new line per message with a lines.txt file
+		}
+		lines = {} -- empty the lines table
+	else -- if the lines command is NOT activated
+		table.insert(lines, content) -- append the message
+	end
+	
+end)
+
+
+client:run("Bot BOT_TOKEN") -- replace BOT_TOKEN with your bot token
