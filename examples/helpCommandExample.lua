8f5ccac6665fb80332a50494e2e8be79e6188b3d
Juan <20613820+PurgePJ@users.noreply.github.com>
2018-05-13 20:04:29 +0200

    New example. (#125)
    * New example.
    
    Had to make this file due to the amount of people asking on how to make a help command to display all the available commands in their bot.
    
    Simple solution and easy to understand.
    
    https://i.imgur.com/rtfmHrj.png
    
    * Update helpCommandExample.lua
    
    * Update helpCommandExample.lua
    
    * Update helpCommandExample.lua
    
    * Update helpCommandExample.lua
    
    * Update helpCommandExample.lua

---
+++ examples/helpCommandExample.lua
@@ -0,0 +1,44 @@
+local discordia = require('discordia')
+local client = discordia.Client()
+discordia.extensions() -- load all helpful extensions
+
+local prefix = "."
+local commands = {
+	ping = {
+		description = "Answers with pong.",
+		exec = function(message)
+			message.channel:send("Pong!")
+		end
+	},
+	hello = {
+		description = "Answers with world.",
+		exec = function(message)
+			message.channel:send("world!")
+		end
+	}
+}
+
+client:on('ready', function()
+	p(string.format('Logged in as %s', client.user.username))
+end)
+
+client:on("messageCreate", function(message)
+	local args = message.content:split(" ") -- split all arguments into a table
+
+	local command = commands[prefix..args[1]]
+	if command then -- ping or hello
+		command.exec(message) -- execute the command
+	end
+
+	if args[1] == prefix.."help" then -- display all the commands
+		local output = {}
+		for word, tbl in pairs(commands) do
+			table.insert(output, "Command: " .. word .. "\nDescription: " .. tbl.description)
+		end
+
+		message:reply(table.concat(output, "\n\n"))
+	end
+end)
+
+
+client:run("Bot BOT_TOKEN") -- replace BOT_TOKEN with your bot token
