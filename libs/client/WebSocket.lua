03bc40c84a40d64b9156ed88357f26af30d20b6e
Nameless <truemedian@gmail.com>
2018-07-12 21:12:39 -0500

    fix 4003 after outage
    fixes 4003 socket errors by not sending the message with no authentication before an identify.

--- libs/client/WebSocket.lua
+++ libs/client/WebSocket.lua
@@ -90,7 +90,11 @@ function WebSocket:_send(op, d)
 	self._mutex:lock()
 	local success, err
 	if self._write then
-		success, err = self._write {opcode = TEXT, payload = encode {op = op, d = d}}
+		if self._session_id then
+			success, err = self._write {opcode = TEXT, payload = encode {op = op, d = d}}
+		else
+			success, err = false, 'Invalid session, sent websocket message without authentication'
+		end
 	else
 		success, err = false, 'Not connected to gateway'
 	end
