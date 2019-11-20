250cdfb74469dfd19da82594de7aabfc17c83946
SinisterRectus <sinister.rectus@gmail.com>
2019-11-20 15:05:44 -0500

    Reduce the default routeDelay to 250 ms

--- libs/client/Client.lua
+++ libs/client/Client.lua
@@ -53,7 +53,7 @@ local GATEWAY_VERSION = constants.GATEWAY_VERSION
 -- do not change these options here
 -- pass a custom table on client initialization instead
 local defaultOptions = {
-	routeDelay = 300,
+	routeDelay = 250,
 	maxRetries = 5,
 	shardCount = 0,
 	firstShard = 0,
