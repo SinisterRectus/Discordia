b0d314e2f146a95ec9588d552a7ad9c8b99914c2
SinisterRectus <sinister.rectus@gmail.com>
2019-12-03 10:31:43 -0500

    Adds more message flag enums

--- libs/enums.lua
+++ libs/enums.lua
@@ -156,9 +156,11 @@ enums.permission = enum {
 }
 
 enums.messageFlag = enum {
-	crossposted         = 0x00000001,
-	isCrosspost         = 0x00000002,
-	suppressEmbeds      = 0x00000004,
+	crossposted          = 0x00000001,
+	isCrosspost          = 0x00000002,
+	suppressEmbeds       = 0x00000004,
+	sourceMessageDeleted = 0x00000008,
+	urgent               = 0x00000010,
 }
 
 enums.actionType = enum {
