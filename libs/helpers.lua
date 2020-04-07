1ce60059b5deea8bd2a82d02f1e24dabfa1171e0
SinisterRectus <sinister.rectus@gmail.com>
2020-04-07 09:43:10 -0400

    Adds benchmark utility

--- libs/helpers.lua
+++ libs/helpers.lua
@@ -1,3 +1,6 @@
+local uv = require('uv')
+
+local hrtime = uv.hrtime
 local insert = table.insert
 local format, byte, gsub = string.format, string.byte, string.gsub
 
@@ -47,9 +50,32 @@ local function checkNumber(obj, base, int, mn, mx)
 	return n
 end
 
+local function benchmark(n, fn, ...)
+
+	local _ = {}
+
+	collectgarbage()
+	collectgarbage()
+	local m1 = collectgarbage('count')
+	local t1 = hrtime()
+
+	for i = 1, n do
+		_[i] = fn(...)
+	end
+
+	collectgarbage()
+	collectgarbage()
+	local m2 = collectgarbage('count')
+	local t2 = hrtime()
+
+	return (m2 - m1) / n, (t2 - t1) / n
+
+end
+
 return {
 	urlEncode = urlEncode,
 	attachQuery = attachQuery,
 	checkNumber = checkNumber,
 	checkType = checkType,
+	benchmark = benchmark,
 }
