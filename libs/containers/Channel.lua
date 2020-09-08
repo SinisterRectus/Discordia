55d9c794075bb4b2af391e57a9834cd26db929b3
SinisterRectus <sinister.rectus@gmail.com>
2020-09-08 00:15:15 -0400

    Compacts Channel class

--- libs/containers/Channel.lua
+++ libs/containers/Channel.lua
@@ -1,9 +1,6 @@
 local Snowflake = require('./Snowflake')
-local GuildChannel = require('../mixins/GuildChannel')
-local GuildTextChannel = require('../mixins/GuildTextChannel')
-local GuildVoiceChannel = require('../mixins/GuildVoiceChannel')
-local TextChannel = require('../mixins/TextChannel')
 
+local json = require('json')
 local class = require('../class')
 
 local format = string.format
@@ -19,16 +16,6 @@ Guild Category = 4
 Guild News     = 5
 Guild Store    = 6
 ]]
-
-class.mixin(Channel, GuildChannel.methods)
-class.mixin(Channel, GuildTextChannel.methods)
-class.mixin(Channel, GuildVoiceChannel.methods)
-class.mixin(Channel, TextChannel.methods)
-
-class.mixin(get, GuildChannel.getters)
-class.mixin(get, GuildTextChannel.getters)
-class.mixin(get, GuildVoiceChannel.getters)
-class.mixin(get, TextChannel.getters)
 
 function Channel:__init(data, client)
 	Snowflake.__init(self, data, client)
@@ -54,9 +41,112 @@ function Channel:_load(data)
 	-- TODO: recipients -- private, group
 end
 
+-- TODO: sorting
+-- TODO: join/leave voice channel
+
 function Channel:delete()
 	return self.client:deleteChannel(self.id)
 end
+
+function Channel:createInvite(payload)
+	return self.client:createChannelInvite(self.id, payload)
+end
+
+function Channel:getInvites()
+	return self.client:getChannelInvites(self.id)
+end
+
+function Channel:getMessage(id)
+	return self.client:getChannelMessage(self.id, id)
+end
+
+function Channel:getFirstMessage()
+	local messages, err = self.client:getChannelMessages(self.id, 1, 'after', self.id)
+	if messages then
+		if messages[1] then -- NOTE: this might not always be an array
+			return messages[1]
+		else
+			return nil, 'Channel has no messages'
+		end
+	else
+		return nil, err
+	end
+end
+
+function Channel:getLastMessage()
+	local messages, err = self.client:getChannelMessages(self.id, 1)
+	if messages then
+		if messages[1] then -- NOTE: this might not always be an array
+			return messages[1]
+		else
+			return nil, 'Channel has no messages'
+		end
+	else
+		return nil, err
+	end
+end
+
+function Channel:getMessages(limit, whence, messageId)
+	return self.client:getChannelMessages(self.id, limit, whence, messageId)
+end
+
+function Channel:getPinnedMessages()
+	return self.client:getPinnedMessages(self.id)
+end
+
+function Channel:bulkDelete(messages)
+	return self.client:bulkDeleteMessages(self.id, messages)
+end
+
+function Channel:triggerTyping()
+	return self.client:triggerTypingIndicator(self.id)
+end
+
+function Channel:send(payload)
+	return self.client:createMessage(self.id, payload)
+end
+
+function Channel:createWebhook(name)
+	return self.client:createWebhook(self.id, name)
+end
+
+function Channel:getWebhooks()
+	return self.client:getChannelWebhooks(self.id)
+end
+
+function Channel:setName(name)
+	return self.client:modifyChannel(self.id, {name = name or json.null})
+end
+
+function Channel:setCategory(parentId)
+	return self.client:modifyChannel(self.id, {parent_id = parentId or json.null})
+end
+
+function Channel:setTopic(topic)
+	return self.client:modifyChannel(self.id, {topic = topic or json.null})
+end
+
+function Channel:enableNSFW()
+	return self.client:modifyChannel(self.id, {nsfw = true})
+end
+
+function Channel:disableNSFW()
+	return self.client:modifyChannel(self.id, {nsfw = false})
+end
+
+function Channel:setRateLimit(limit)
+	return self.client:modifyChannel(self.id, {rate_limit_per_user = limit or json.null})
+end
+
+function Channel:setBitrate(bitrate)
+	return self.client:modifyChannel(self.id, {bitrate = bitrate or json.null})
+end
+
+function Channel:setUserLimit(userLimit)
+	return self.client:modifyChannel(self.id, {user_limit = userLimit or json.null})
+end
+
+----
 
 function get:type()
 	return self._type
@@ -64,6 +154,42 @@ end
 
 function get:mentionString()
 	return format('<#%s>', self.id)
+end
+
+function get:name()
+	return self._name
+end
+
+function get:position()
+	return self._position
+end
+
+function get:guildId()
+	return self._guild_id
+end
+
+function get:parentId()
+	return self._parent_id
+end
+
+function get:topic()
+	return self._topic
+end
+
+function get:nsfw()
+	return self._nsfw
+end
+
+function get:rateLimit()
+	return self._rate_limit_per_user or 0
+end
+
+function get:bitrate()
+	return self._bitrate
+end
+
+function get:userLimit()
+	return self._user_limit
 end
 
 return Channel
