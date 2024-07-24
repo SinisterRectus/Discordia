8f4ec1c48d8b7b295fce7413bf3e5facacab1f56
Troptop <87533146+NickIsADev@users.noreply.github.com>
2024-07-24 13:00:40 -0400

    Add new getter: get.badges()

--- libs/containers/User.lua
+++ libs/containers/User.lua
@@ -7,8 +7,13 @@ specific guild's context.
 local Snowflake = require('containers/abstract/Snowflake')
 local FilteredIterable = require('iterables/FilteredIterable')
 local constants = require('constants')
+local enums = require('enums')
+local bit = require('bit')
 
+local band = bit.band
 local format = string.format
+local insert = table.insert
+
 local DEFAULT_AVATARS = constants.DEFAULT_AVATARS
 
 local User, get = require('class')('User', Snowflake)
@@ -200,6 +205,20 @@ function get.mutualGuilds(self)
 		end)
 	end
 	return self._mutual_guilds
+end
+
+--[=[@p badges Array An array of all badges the user has, represented by the badge's name.]=]
+function get.badges(self)
+	local badges = {}
+	local publicflags = self._public_flags
+
+	for badge, flag in pairs(enums.userFlag) do
+		if band(publicflags, flag) == flag then
+			insert(badges, badge)
+		end
+	end
+	
+	return badges
 end
 
 return User
